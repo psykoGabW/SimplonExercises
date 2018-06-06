0168b7ce41aa5ebeb0f4ab11b33731fb9204a684
gabrielW <gabriel.wisniewski@gmail.com>
2018-06-06 09:36:14 +0200

    MAJ Fin Session 4

--- Session4/springTest/src/main/java/co/simplon/gwi/spring/exo01/SpringTestApplication.java
+++ Session4/springTest/src/main/java/co/simplon/gwi/spring/exo01/SpringTestApplication.java
@@ -1,12 +1,22 @@
 package co.simplon.gwi.spring.exo01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages= {"co.simplon.gwi.spring.exo01","co.simplon.repository"})
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner runner() {
+		return args ->{
+			// Instructions que l'on souhaite voir exécuter à chaque démarrage de l'application.
+			// Initialisation d'objets etc....
+		};
 	}
 }
